5922eb94f0d06335a90fb027ad2edafa0dab7d4e
Ta Chi Nguyen <tcnguyen93@gmail.com>
2024-11-02 21:05:18 +0700

    up

---
+++ CompassnSpace/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CompassnSpace
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CompassnSpace/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CompassnSpace
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
